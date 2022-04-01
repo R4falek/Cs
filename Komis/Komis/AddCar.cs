b052b972d5937d6ba56605bf286c50e0e31e3c5c
R4falek <72551023+R4falek@users.noreply.github.com>
2022-04-01 09:53:54 +0200

    Add files via upload

---
+++ Komis/Komis/AddCar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Komis
+{
+    public partial class AddCar : Form
+    {
+        private static bool exist = false;
+        public AddCar()
+        {
+            InitializeComponent();
+            exist = true;
+        }
+
+        public static bool GetExist() { return exist; }
+    }
+}
